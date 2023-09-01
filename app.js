ee732fc4f332363a6aa0ddf26a2e7d8f472b8861
piyushb790 <piyushb790@gmail.com>
2023-09-01 21:12:48 +0530

    laying the foundation

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: 1 }, "I am H1"),
-    React.createElement("h2", { key: 2 }, "I am H2"),
-  ]),
-  React.createElement(
-    "div",
-    { id: "child2" },
+const Heading2 = () => {
+  return (
+    <div>
+      <Title />
+      {Title()}
+      {num}
+      {Heading}
 
-    [
-      React.createElement("h1", { key: 3 }, "I am again h1"),
-      React.createElement("h2", { key: 4 }, "I am again h2"),
-    ]
-  )
+      <h1>React functional component with return</h1>
+    </div>
+  );
+};
+
+const Title = () => <h1>Namaste React using JSX 🚀</h1>;
+
+const Heading = <h1>React element component</h1>;
+
+console.log(Heading);
+let num = 1000;
+
+const description = (
+  <div>
+    <Heading2 />
+    <h1>This is description</h1>
+  </div>
 );
-console.log(parent);
 
-const heading = React.createElement(
-  "h1",
-  { className: "myHeadin" },
-  "Namaste React "
-);
-
-// console.log(heading);
+//React Component
+//* class based component
+//* Functional component - is normal javascript function which return some peice of jsx
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(description);
